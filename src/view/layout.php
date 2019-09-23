68c53e4189cf70b97f138075731a1c6c85b7c5ec
Eva Denys <eva.denys@student.howest.be>
2019-09-23 20:08:46 +0200

    test

--- src/view/layout.php
+++ src/view/layout.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="icon" href="assets/img/Large/about.png">
-    <title>E.D.</title>
+    <title>E.D.E</title>
     <?php echo $css;?>
 
   </head>
