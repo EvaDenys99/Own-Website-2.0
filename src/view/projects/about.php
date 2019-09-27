cfaf6482d477936b6ac3483404e2d57ac700644b
Eva Denys <eva.denys@student.howest.be>
2019-09-27 21:16:17 +0200

    About page styling complete

--- src/view/projects/about.php
+++ src/view/projects/about.php
@@ -1,38 +1,50 @@
 <section >
-  <h2 >Development. Design. Experience.</h2>
-  <img src="" alt="">
-  <img src="" alt="">
-  <div class="about">
-    <div class="info">
-      <p class="info_3words">Those are my three keywords.</p>
-      <p class="info_mini">Initially I have a background in <span> ICT & Engineering</span>. This is where my passion for developing began and although building robots is fun, developing websites is even better. </p>
-      <p>But why those three keywords, you might ask?
-      Let’s start with <span>development</span>. Making sure your idea is realized and let the static design come to life is my main goal. As a web developer, I’m at ease with a lot of coding languages to do exactly that.
-      Besides being a developer, I also appreciate a good-looking <span> design</span>. This is why I’m a designer on the side to make sure your site is a delight to the eye.
-      Last but not least: <span>experience</span>. Making sure you and your audience have the experience they want and expect is essential in both developing and designing a website.</p>
-      <p>If interested, you can always check out  <a href="" class="link"> my resume </a>  or you can always contact me directly <a href="" class="link"> evadenys@hotmail.com</a> . </p>
+  <div class="boven">
+    <div class="heardbeat2.0">
+      <h2 class="title_page2">Development. Design. Experience.</h2>
+      <img class="title_page2_img" src="assets/img/projecten/heartbeat_about.png" alt="">
+
+      <div class="about">
+        <div class="info">
+          <p class="info_3words">Those are my three keywords.</p>
+          <p class="info_mini">Initially I have a background in <span> ICT & Engineering</span>. This is where my passion for developing began and although building robots is fun, developing websites is even better. </p>
+          <p class="info_mini">But why those three keywords, you might ask?
+          Let’s start with <span>development</span>. Making sure your idea is realized and let the static design come to life is my main goal. As a web developer, I’m at ease with a lot of coding languages to do exactly that.
+          Besides being a developer, I also appreciate a good-looking <span> design</span>. This is why I’m a designer on the side to make sure your site is a delight to the eye.
+          Last but not least: <span>experience</span>. Making sure you and your audience have the experience they want and expect is essential in both developing and designing a website.</p>
+          <p>If interested, you can always check out  <a href="" class="link"> my resume </a>  or you can always contact me directly <a href="" class="link"> evadenys@hotmail.com</a> . </p>
+        </div>
+
+        <div class="skills">
+          <div>
+            <h3>Development</h3>
+            <p class="skillset">HTML - CSS - JavaScript - React - Arduino - Dwengo - PLC</p>
+          </div>
+          <div>
+            <h3>Design</h3>
+            <p class="skillset">Photoshop - Illustrator - Sketch - After effects</p>
+          </div>
+          <div>
+            <h3>Research</h3>
+            <p class="skillset">UX - Persona's - Adobe XD</p>
+          </div>
+        </div>
+      </div>
     </div>
 
-    <div class="skills">
-      <div>
-        <h3>Development</h3>
-        <p class="skillset">HTML - CSS - JavaScript - React - Arduino - Dwengo - PLC</p>
-      </div>
-      <div>
-        <h3>Design</h3>
-        <p class="skillset">Photoshop - Illustrator - Sketch - After effects</p>
-      </div>
-      <div>
-        <h3>Research</h3>
-        <p class="skillset">UX - Persona's - Adobe XD</p>
+    <div>
+      <!-- <img class="me" src="assets/img/projecten/me.png" width="460" height="612"  alt=""> -->
+      <div class="me"></div>
+      <div class="socialMedia">
+        <a href=""><img class="socialMedia_item" src="assets/img/projecten/fb.png" alt=""></a>
+        <a href=""><img class="socialMedia_item" src="assets/img/projecten/be.png" alt=""></a>
+        <a href=""><img class="socialMedia_item" src="assets/img/projecten/git.png" alt=""></a>
       </div>
     </div>
   </div>
 
-  <section class="socialMedia">
-    <a href="">facebook</a>
-    <a href="">behance</a>
-    <a href="">github</a>
-  </section>
+
+
+
 </section>
 
