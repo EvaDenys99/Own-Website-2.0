a331a154d06b668d0c4b448a7c66e78feeadb6fe
Eva Denys <eva.denys@student.howest.be>
2019-09-27 20:05:22 +0200

    More styling

--- src/view/projects/about.php
+++ src/view/projects/about.php
@@ -1,28 +1,35 @@
-<section>
-  <h2>Development. Design. Experience.</h2>
+<section >
+  <h2 >Development. Design. Experience.</h2>
   <img src="" alt="">
-  <p>Those are my three keywords.</p>
-  <p>Initially I have a background in <span> ICT & Engineering</span>. This is where my passion for developing began and although building robots is fun, developing websites is even better. </p>
-  <p>But why those three keywords, you might ask?
-  Let’s start with <span>development</span>. Making sure your idea is realized and let the static design come to life is my main goal. As a web developer, I’m at ease with a lot of coding languages to do exactly that.
-  Besides being a developer, I also appreciate a good-looking <span> design</span>. This is why I’m a designer on the side to make sure your site is a delight to the eye.
-  Last but not least: <span>experience</span>. Making sure you and your audience have the experience they want and expect is essential in both developing and designing a website.</p>
-  <p>If interested, you can always check out <span> my resume </span> or you can always contact me directly <span> evadenys@hotmail.com </span>. </p>
-  <section>
-    <div>
-      <h3>Development</h3>
-      <p>HTML - CSS - JavaScript - React - Arduino - Dwengo - PLC</p>
+  <img src="" alt="">
+  <div class="about">
+    <div class="info">
+      <p class="info_3words">Those are my three keywords.</p>
+      <p class="info_mini">Initially I have a background in <span> ICT & Engineering</span>. This is where my passion for developing began and although building robots is fun, developing websites is even better. </p>
+      <p>But why those three keywords, you might ask?
+      Let’s start with <span>development</span>. Making sure your idea is realized and let the static design come to life is my main goal. As a web developer, I’m at ease with a lot of coding languages to do exactly that.
+      Besides being a developer, I also appreciate a good-looking <span> design</span>. This is why I’m a designer on the side to make sure your site is a delight to the eye.
+      Last but not least: <span>experience</span>. Making sure you and your audience have the experience they want and expect is essential in both developing and designing a website.</p>
+      <p>If interested, you can always check out  <a href="" class="link"> my resume </a>  or you can always contact me directly <a href="" class="link"> evadenys@hotmail.com</a> . </p>
     </div>
-    <div>
-      <h3>Design</h3>
-      <p>Photoshop - Illustrator - Sketch - After effects</p>
+
+    <div class="skills">
+      <div>
+        <h3>Development</h3>
+        <p class="skillset">HTML - CSS - JavaScript - React - Arduino - Dwengo - PLC</p>
+      </div>
+      <div>
+        <h3>Design</h3>
+        <p class="skillset">Photoshop - Illustrator - Sketch - After effects</p>
+      </div>
+      <div>
+        <h3>Research</h3>
+        <p class="skillset">UX - Persona's - Adobe XD</p>
+      </div>
     </div>
-    <div>
-      <h3>Research</h3>
-      <p>UX - Persona's - Adobe XD</p>
-    </div>
-  </section>
-  <section>
+  </div>
+
+  <section class="socialMedia">
     <a href="">facebook</a>
     <a href="">behance</a>
     <a href="">github</a>
