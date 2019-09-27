96b89f8e49a6d736d5f0b293488501fc561d8121
Eva Denys <eva.denys@student.howest.be>
2019-09-27 22:09:41 +0200

    About page

--- src/view/projects/about.php
+++ src/view/projects/about.php
@@ -6,11 +6,11 @@
 
       <div class="about">
         <div class="info">
-          <p class="info_3words">Those are my three keywords.</p>
+          <p class="info_mini">Those are my three keywords.</p>
           <p class="info_mini">Initially I have a background in <span> ICT & Engineering</span>. This is where my passion for developing began and although building robots is fun, developing websites is even better. </p>
-          <p class="info_mini">But why those three keywords, you might ask?
-          Let’s start with <span>development</span>. Making sure your idea is realized and let the static design come to life is my main goal. As a web developer, I’m at ease with a lot of coding languages to do exactly that.
-          Besides being a developer, I also appreciate a good-looking <span> design</span>.<br> This is why I’m a designer on the side to make sure your site is a delight to the eye.<br>
+          <p class="info_mini">But why those three keywords, you might ask? <br>
+          Let’s start with <span>development</span>. Making sure your idea is realized and let the static design come to life is my main goal. As a web developer, I’m at ease with a lot of coding languages to do exactly that.<br>
+          Besides being a developer, I also appreciate a good-looking <span> design</span>. This is why I’m a designer on the side to make sure your site is a delight to the eye.<br>
           Last but not least: <span>experience</span>. Making sure you and your audience have the experience they want and expect is essential in both developing and designing a website.</p>
           <p>If interested, you can always check out  <a href="" class="link"> my resume </a>  or you can always contact me directly <a href="mailto:connect.now.ed@hotmail.com" class="link"> evadenys@hotmail.com</a> . </p>
         </div>
@@ -33,12 +33,11 @@
     </div>
 
     <div>
-      <!-- <img class="me" src="assets/img/projecten/me.png" width="460" height="612"  alt=""> -->
       <div class="me"></div>
       <div class="socialMedia">
-        <a target="_blank" href="https://www.facebook.com/eva.denys.9"><img class="socialMedia_item" src="assets/img/projecten/fb.png" alt=""></a>
-        <a target="_blank" href="https://www.behance.net/_eva_denys_/"><img class="socialMedia_item" src="assets/img/projecten/be.png" alt=""></a>
-        <a target="_blank" href="https://github.com/EvaDenys99"><img class="socialMedia_item" src="assets/img/projecten/git.png" alt=""></a>
+        <a target="_blank" href="https://www.facebook.com/eva.denys.9"><img class="socialMedia_item" src="assets/img/fb.png" alt=""></a>
+        <a target="_blank" href="https://www.behance.net/_eva_denys_/"><img class="socialMedia_item" src="assets/img/be.png" alt=""></a>
+        <a target="_blank" href="https://github.com/EvaDenys99"><img class="socialMedia_item" src="assets/img/git.png" alt=""></a>
       </div>
     </div>
   </div>
