388b8cc15338ec701a5e4f4b85032536cf943821
Eva Denys <eva.denys@student.howest.be>
2019-10-01 23:42:18 +0200

    Most of the css is done - not responsive

--- src/view/layout.php
+++ src/view/layout.php
@@ -4,8 +4,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="icon" href="assets/img/flavi.png">
-    <title>E.D. Portfolio</title>
+  <link rel="icon" href="assets/img/flavi02.png">
+    <title>Portfolio</title>
 
     <?php echo $css;?>
 
--- src/view/projects/view.php
+++ src/view/projects/view.php
@@ -1,46 +1,60 @@
-<section class="project__detail">
-  <img src="././assets/img/projecten/<?php echo $project["name"] ?>/<?php echo $project["detailMainImg"]?>.png" alt="">
-  <img src="././assets/img/heartbeat_about.png" alt="">
-  <h2><?php echo $project["name"] ?></h2>
-  <p><?php echo $project["tags"] ?></p>
-  <div>
-    <h3>client</h3>
-    <?php echo $project["client"] ?>
+<section>
+  <div class="project__detail">
+    <img class="project__detail__img" src="././assets/img/projecten/<?php echo $project["name"] ?>/<?php echo $project["mainPic"]?>.png" alt="">
+    <div class="project__detail__title">
+      <img class="project__detail__img__hart" src="././assets/img/heartbeat_about.png" alt="">
+      <h2 class="project__detail__title__name"><?php echo $project["name"] ?></h2>
+      <p><?php echo $project["tags"] ?></p>
+    </div>
+    <div class="project__detail__mini00">
+      <div class="project__detail__mini01">
+        <div class="project__detail__mini">
+            <div>
+              <h3>client</h3>
+              <?php echo $project["client"] ?>
+            </div>
+            <div>
+              <h3>team</h3>
+              <?php echo $project["team"] ?>
+            </div>
+            <div>
+              <h3>role</h3>
+              <?php echo $project["role"] ?>
+            </div>
+            <div>
+              <h3>date</h3>
+              <?php echo $project["date"] ?>
+            </div>
+        </div>
+        <div>
+          <h3>all to know ...</h3>
+          <?php echo $project["bio"] ?>
+        </div>
+      </div>
+    </div>
+    <div class="detail__button">
+      <?php if(!empty ($project["link"])): ?>
+        <a target="_blank" class="project__detail__button" href="<?php echo $project['link'];?>">Go look!</a>
+      <?php endif; ?>
+    </div>
   </div>
-  <div>
-    <h3>team</h3>
-    <?php echo $project["team"] ?>
-  </div>
-  <div>
-    <h3>role</h3>
-    <?php echo $project["role"] ?>
-  </div>
-  <div>
-    <h3>date</h3>
-    <?php echo $project["date"] ?>
-  </div>
-  <div>
-    <h3>all to know ...</h3>
-    <?php echo $project["bio"] ?>
-  </div>
-  <div>
-   <?php if(!empty ($project["link"])): ?>
-    <a target="_blank" href="<?php echo $project['link'];?>">Go look!</a>
-   <?php endif; ?>
-  </div>
-  <div>
-    <?php $iets =explode(", ", $project["img"]) ?>
+
+  <div class="project__detail__fotos__videos">
+      <?php $iets =explode(", ", $project["img"]) ?>
       <?php foreach($iets as $iet): ?>
-      <img src="././assets/img/projecten/<?php echo $project["name"]?>/<?php echo $iet?>.png" alt="">
-    <?php endforeach; ?>
+        <div class="project__detail__foto">
+          <img width="202" height="202" src="././assets/img/projecten/<?php echo $project["name"]?>/<?php echo $iet?>.png" alt="">
+        </div>
+      <?php endforeach; ?>
+      <?php if(!empty ($project["video"])): ?>
+        <div>
+          <a  href="<?php echo $project['link'];?>">Go look!</a>
+        </div>
+      <?php endif; ?>
   </div>
-  <div>
-   <?php if(!empty ($project["video"])): ?>
-    <a  href="<?php echo $project['link'];?>">Go look!</a>
-   <?php endif; ?>
-  </div>
-  <div class="footerDetail">
-    <a class="footerDetail__text" href="index.php?page=index">X Close Project</a>
-  </div>
+
+  <a class="footerDetail" href="index.php?page=index">
+    X Close Project
+  </a>
 </section>
 
